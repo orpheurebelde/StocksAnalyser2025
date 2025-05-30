eaaac642249b5c926bc759c75bca30d01fe41f12
orpheurebelde <almeida1976marco@gmail.com>
2025-05-30 14:44:16 +0100

    Update 3_📊_Market_Analysis.py

--- pages/3_📊_Market_Analysis.py
+++ pages/3_📊_Market_Analysis.py
@@ -273,7 +273,7 @@ with st.expander("📈 Monthly Performance Analysis", expanded=True):
                 color = 'red'
             else:
                 color = 'orange'
-            st.markdown(f"<span style='color:{color}; font-size:18px;'><strong>Current Perfomance</strong>: {current_performance * 100:.2f}%</span>",unsafe_allow_html=True)
+            st.markdown(f"<span style='color:{color}; font-size:18px;'><strong>Current Year Perfomance</strong>: {current_performance * 100:.2f}%</span>",unsafe_allow_html=True)
             #st.write(f"**Current Year Performance**: {current_performance * 100:.2f}%")
             st.write(f"**Historical Max Yearly Return**: {historical_max * 100:.2f}%")
             st.write(f"**Historical Min Yearly Return**: {historical_min * 100:.2f}%")
