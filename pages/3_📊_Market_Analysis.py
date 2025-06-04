1e0c19f6f5f8313ff3673d406bc8bef05ec73978
orpheurebelde <almeida1976marco@gmail.com>
2025-06-04 12:17:12 +0100

    Update 3_📊_Market_Analysis.py

--- pages/3_📊_Market_Analysis.py
+++ pages/3_📊_Market_Analysis.py
@@ -173,7 +173,7 @@ def show_indicators(ticker, title):
     <div><strong>Fibonacci Level (3Y Range)</strong>: {fib_level_3y:.2f}% - {fib_comment_3y}</div>
     <div><strong>Fibonacci Level (5Y Range)</strong>: {fib_level_5y:.2f}%</div>
     <div><strong>Fibonacci Level (10Y Range)</strong>: {fib_level_10y:.2f}%</div>
-
+    <hr style='border: 1px solid #444;' />
     </div>
     """, unsafe_allow_html=True)
 
